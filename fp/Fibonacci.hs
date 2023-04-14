161d6f1ceec66f7fccbef93939535c5edb81b4b5
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2023-04-13 22:30:59 -0500

    [ fibonacci ] where -> let.

--- fp/Fibonacci.hs
+++ fp/Fibonacci.hs
@@ -10,12 +10,12 @@ fib n = fib (n - 1) + fib (n - 2)
 -- Accumulator pattern version. Adapted from Fig. 5.17 in Kent D. Lee
 -- (2017), Foundations of Programming Languages, 2nd ed.
 fibAP :: Natural -> Natural
-fibAP n = fibHelper 0 1 0
-  where
-  fibHelper :: Natural -> Natural -> Natural -> Natural
-  fibHelper count current previous =
-    if count == n then previous
-    else fibHelper (count + 1) (previous + current) current
+fibAP n =
+  let fibHelper :: Natural -> Natural -> Natural -> Natural
+      fibHelper count current previous =
+        if count == n then previous
+        else fibHelper (count + 1) (previous + current) current
+  in  fibHelper 0 1 0
 
 main :: IO ()
-main = print $ fib 42
+main = print $ fibAP 42
