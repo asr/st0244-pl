6982e8ebb7df8c583fced6f1773e3be1289d6977
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2023-10-03 14:50:41 -0500

    [ fp ] Cosmetic changes.

--- fp/Tuples.hs
+++ fp/Tuples.hs
@@ -7,6 +7,6 @@ appendC []       ys = ys
 appendC (x : xs) ys = x : appendC xs ys
 
 -- Concatenation of lists using tuples.
-appendT :: ([a],[a]) -> [a]
+appendT :: ([a], [a]) -> [a]
 appendT ([], ys)       = ys
-appendT ((x : xs), ys) = x : appendT (xs,ys)
+appendT ((x : xs), ys) = x : appendT (xs, ys)
