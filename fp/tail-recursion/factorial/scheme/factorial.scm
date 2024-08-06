735862c6a7b7af4b292d52e83a2b9c63f43a7310
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2024-08-06 11:33:42 -0500

    [ fp ] Fixed typos.

--- fp/tail-recursion/factorial/scheme/factorial.scm
+++ fp/tail-recursion/factorial/scheme/factorial.scm
@@ -1,11 +1,11 @@
 ; Non-tail recursive factorial
-(define (fact-tr n)
+(define (fact-ntr n)
   (if (= n 1)
       1
-      (* n (fact-tr (- n 1)))))
+      (* n (fact-ntr (- n 1)))))
 
 ; Tail recursive factorial
-(define (fact-ntr n result)
+(define (fact-tr n result)
   (if (= n 1)
       result
-      (fact-ntr (- n 1) (* n result))))
+      (fact-tr (- n 1) (* n result))))
