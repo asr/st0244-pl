dadf83fbbc33059e97883dfed3505269b6a5c747
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2024-10-18 07:00:43 -0500

    [ Java ] Added example on complex numbers.

---
+++ oop/complex-numbers/java/Complex.java
@@ -0,0 +1,50 @@
+// Adapted from:
+// Kenneth C. Louden y Kenneth A. Lambert (2011). Programming
+// Languages. Principles and Practice. 3rd ed. Cengage Learning.
+
+public class Complex extends Object{
+
+  // Instance variables
+  private double re, im;
+
+  // Default constructor
+  public Complex(){
+    this(0, 0);
+  }
+
+  // Parameterized constructor
+  public Complex(double realPart, double imaginaryPart){
+    re = realPart;
+    im = imaginaryPart;
+  }
+
+  public double realPart(){
+    return re;
+  }
+
+  public double imaginaryPart(){
+    return im;
+  }
+
+
+  public boolean equals(Complex c){
+    return (this.realPart() == c.realPart() &&
+            this.imaginaryPart() == c.imaginaryPart());
+  }
+
+  public Complex add(Complex c){
+    return new Complex(this.realPart() + c.realPart(),
+                       this.imaginaryPart() + c.imaginaryPart());
+  }
+
+  public Complex multiply(Complex c){
+    return new Complex(this.realPart() * c.realPart() -
+                       this.imaginaryPart() * c.imaginaryPart(),
+                       this.realPart() * c.imaginaryPart() +
+                       this.imaginaryPart() * c.realPart());
+  }
+
+  public String toString(){
+    return this.realPart() + "+" + this.imaginaryPart() + "i";
+  }
+}
---
+++ oop/complex-numbers/java/TestComplex.java
@@ -0,0 +1,15 @@
+// Adapted from:
+// Kenneth C. Louden y Kenneth A. Lambert (2011). Programming
+// Languages. Principles and Practice. 3rd ed. Cengage Learning.
+
+public class TestComplex{
+  static public void main(String[] args){
+    Complex c1 = new Complex(3.5, 4.6);
+    Complex c2 = new Complex(2.0, 2.0);
+
+    System.out.println(c1.equals(c2));
+
+    Complex sum = c1.add(c2);
+    System.out.println(sum);
+  }
+}
